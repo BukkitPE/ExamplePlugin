ab17c965b9750d307fd4229d08bf7226ca1d1fda
Andre <andrethegamer911@gmail.com>
2016-02-01 11:56:14 -0800

    Created ExamplePlugin

---
+++ src/ExamplePlugin/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ExamplePlugin;
+
+use BukkitPE\plugin\PluginBase;
+use BukkitPE\event\Listener;
+use BukkitPE\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable() {
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function onJoin(PlayerJoinEvent $event){
+
+$event->getPlayer()->sendMessage("Welcome to my server");
+}
+}
