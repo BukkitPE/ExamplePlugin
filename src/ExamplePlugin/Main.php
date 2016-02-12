fe3c0cddc0f090d999b946707d90d8f9c3d34ccc
Andre <andrethegamer911@gmail.com>
2016-02-12 11:56:28 -0500

    Added comments

--- src/ExamplePlugin/Main.php
+++ src/ExamplePlugin/Main.php
@@ -14,6 +14,6 @@ $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
 public function onJoin(PlayerJoinEvent $event){
 
-$event->getPlayer()->sendMessage("Welcome to my server");
+$event->getPlayer()->sendMessage("Welcome to my server");  // this will send the player that joined the server the message inside the "()"
 }
 }
